5e8c719b317bb240452db02eb65f7714a6f00b1e
Evgenia <128784367+varinskaevg@users.noreply.github.com>
2024-05-27 08:31:30 +0300

    Add files via upload

---
+++ documents/Varinska202-TK/prj-1/todo-gr4-go/internal/infra/http/controllers/task_controller.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/app"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/domain"
+)
+
+type TaskController struct {
+	taskService app.TaskService
+}
+
+func NewTaskController(ts app.TaskService) TaskController {
+	return TaskController{
+		taskService: ts,
+	}
+}
+
+func (c TaskController) Save() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user := r.Context().Value(UserKey).(domain.User)
+
+	}
+}
